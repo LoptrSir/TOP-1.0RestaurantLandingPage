170ee2fa07aea4895a395093a78ca36ed924954a
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2023-11-07 17:23:00 -0700

    Info.txt 11/7/1721

---
+++ src/module.js
@@ -0,0 +1,15 @@
+const myFooter = () => {
+    const footer = document.createElement('div');
+    footer.style.backgroundColor = '#333';
+    footer.style.fontSize = '0.6rem';
+    footer.style.color = '#f8afe5';
+    footer.style.padding = '3px';
+    footer.style.textAlign = 'center';
+    footer.style.position = 'fixed';
+    footer.style.width = '100%';
+    footer.style.bottom = '0';
+    footer.innerHTML = 'loptrSir';
+    return footer;
+    };
+    
+    export default myFooter;
